39307a7975f93cb606c0ff3a9f5efdc5495e94cd
yui-szk <yuisuzuki.017@gmail.com>
2023-11-13 00:59:32 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { TodoList } from "./TodoList";
+
+function App() {
+  return (
+    <div style={{ margin: "2em" }}>
+      <TodoList />
+    </div>
+  );
+}
