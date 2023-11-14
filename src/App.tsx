a34bb5036123aa84272da2d88f3c6994f80281c1
yui-szk <yuisuzuki.017@gmail.com>
2023-11-15 01:14:07 +0900

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import { useState } from "react";
-import { TodoList } from "./TodoList";
 
-function App() {
+type Todo = {
+  value: string;
+};
+
+export function App() {
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const handleSubmit = () => {
+    if (!text) return;
+    const newTodo: Todo = {
+      value: text,
+    };
+    setTodos((todos) => [newTodo, ...todos]);
+    setText("");
+  };
   return (
-    <div style={{ margin: "2em" }}>
-      <TodoList />
+    <div>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <input
+          type="submit"
+          value="追加"
+          onSubmit={(e) => e.preventDefault()}
+        />
+      </form>
+      <p>{text}</p>
     </div>
   );
 }
