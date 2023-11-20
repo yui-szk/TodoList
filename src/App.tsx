89f3d4c3877e0ef87595f576352967a3a08c1279
yui-szk <yuisuzuki.017@gmail.com>
2023-11-20 10:48:16 +0900

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,132 @@ import { useState } from "react";
 
 type Todo = {
   value: string;
+  readonly id: number;
+  checked: boolean;
+  removed: boolean;
 };
+
+type Filter = "all" | "checked" | "unchecked" | "removed";
 
 export function App() {
   const [text, setText] = useState("");
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [filter, setFilter] = useState<Filter>("all");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
   const handleSubmit = () => {
     if (!text) return;
+
     const newTodo: Todo = {
       value: text,
+      id: new Date().getTime(),
+      checked: false,
+      removed: false,
     };
+
     setTodos((todos) => [newTodo, ...todos]);
     setText("");
   };
+
+  const handleTodo = <K extends keyof Todo, V extends Todo[K]>(
+    id: number,
+    key: K,
+    value: V
+  ) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, [key]: value };
+        } else {
+          return todo;
+        }
+      });
+      return newTodos;
+    });
+  };
+
+  const handleSort = (filter: Filter) => {
+    setFilter(filter);
+  };
+
+  const handleEmpty = () => {
+    setTodos((todos) => todos.filter((todo) => !todo.removed));
+  };
+
+  const filteredTodos = todos.filter((todo) => {
+    switch (filter) {
+      case "all":
+        return !todo.removed;
+      case "checked":
+        return todo.checked && !todo.removed;
+      case "unchecked":
+        return !todo.checked && !todo.removed;
+      case "removed":
+        return todo.removed;
+      default:
+        return todo;
+    }
+  });
+
   return (
     <div>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input
-          type="text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <input
-          type="submit"
-          value="追加"
-          onSubmit={(e) => e.preventDefault()}
-        />
-      </form>
+      <select
+        defaultValue="all"
+        onChange={(e) => handleSort(e.target.value as Filter)}
+      >
+        <option value="all">All Tasks</option>
+        <option value="checked">Completed Tasks</option>
+        <option value="unchecked">Current Tasks</option>
+        <option value="removed">Dustbin</option>
+      </select>
+      {filter === "removed" ? (
+        <button
+          onClick={handleEmpty}
+          disabled={todos.filter((todo) => todo.removed).length === 0}
+        >
+          Clear Dustbin
+        </button>
+      ) : (
+        filter !== "checked" && (
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
+            }}
+          >
+            <input type="text" value={text} onChange={(e) => handleChange(e)} />
+            <input type="submit" value="Add" onSubmit={handleSubmit} />
+          </form>
+        )
+      )}
+      <ul>
+        {filteredTodos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <input
+                type="checkbox"
+                disabled={todo.removed}
+                checked={todo.checked}
+                onChange={() => handleTodo(todo.id, "checked", !todo.checked)}
+              />
+              <input
+                type="text"
+                disabled={todo.checked || todo.removed}
+                value={todo.value}
+                onChange={(e) => handleTodo(todo.id, "value", e.target.value)}
+              />
+              <button
+                onClick={() => handleTodo(todo.id, "removed", !todo.removed)}
+              >
+                {todo.removed ? "Restore" : "Delete"}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
       <p>{text}</p>
     </div>
   );
